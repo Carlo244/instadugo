93a82153f2689150f03e3a17e0867920ee2de9a1
Carloooo <113890894+Carlo244@users.noreply.github.com>
2026-05-05 21:27:16 +0800

    fix(cloud): revert broadcasts to queued for Cloud compatibility

--- app/Events/DonationCreated.php
+++ app/Events/DonationCreated.php
@@ -6,11 +6,11 @@ use App\Models\Donation;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class DonationCreated implements ShouldBroadcastNow
+class DonationCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
